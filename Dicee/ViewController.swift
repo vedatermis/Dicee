7fa6e20e3d4b7f9199384aceb51c0437865f3a55
Vedat ERMIS <vedatermis@hotmail.com>
2019-07-21 11:05:01 +0300

    commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Vedat ERMIS on 20.07.2019.
+//  Copyright © 2019 Vedat ERMIS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
